13e0fc1746add71cbe7bd53ec8a6365d3c5b46ca
suleyman <jdksuleyman@gmail.com>
2021-08-24 00:50:08 +0300

    slider ve hizmet veritabanlarına erişme sağlandi

--- KurumsalWeb/Controllers/HomeController.cs
+++ KurumsalWeb/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KurumsalWeb.Models.DataContext;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,21 @@ namespace KurumsalWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private KurumsalDBContext db = new KurumsalDBContext();
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.Hizmetler = db.Hizmet.ToList().OrderByDescending(x=>x.HizmetId);
             return View();
+        }
+        public ActionResult SliderPartial()
+        {
+            return View(db.Slider.ToList().OrderByDescending(x => x.SliderId));
+
+        }
+        public ActionResult HizmetPartial()
+        {
+            return View(db.Hizmet.ToList());
         }
     }
 }
